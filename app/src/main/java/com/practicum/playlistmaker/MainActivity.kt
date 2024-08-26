1a8d257fcc3c6f8a892fd2b254efb99bc3794542
Panchenko Zakhar <zakhar-panchenko@yandex.ru>
2024-08-26 19:12:57 +0300

    Убраны неиспользуемые директивы импорта

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -3,9 +3,7 @@ package com.practicum.playlistmaker
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
